4900a8fb0684dd8ed2aa643f08739e7f021aca37
carson <carsonfrankovich6@gmail.com>
2024-01-01 20:14:40 -0500

    remove example

--- src/sm_example/src/sm_example_node.cpp
+++
@@ -1,9 +0,0 @@
-#include <rclcpp/rclcpp.hpp>
-#include <sm_example/sm_example.hpp>
-
-int main(int argc, char **argv)
-{
-    rclcpp::init(argc, argv);
-    smacc2::run<sm_example::SmExample>();
-    return 0;
-}
